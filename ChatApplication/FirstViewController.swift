d127fb10452545466e0f0bb1f15006991d9c4f20
GeorgiSwiftDeveloper <malkhasyangeorge@gmail.com>
2018-11-17 19:24:15 -0800

    Initial Commit

---
+++ ChatApplication/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChatApplication
+//
+//  Created by Georgi Malkhasyan on 11/17/18.
+//  Copyright © 2018 Adamyan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ChatApplication/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChatApplication
+//
+//  Created by Georgi Malkhasyan on 11/17/18.
+//  Copyright © 2018 Adamyan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
